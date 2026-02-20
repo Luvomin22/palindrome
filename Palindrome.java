a282e6e255312099ba3f514c29be5abd56159617
Luvomin22 <24124027@srcas.ac.in>
2026-02-20 11:52:38 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,21 +1,26 @@
-import java.util.Scanner;
 public class Palindrome {
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-System.out.print("Enter a number: ");
-int number = sc.nextInt();
-int original = number;
-int reverse = 0;
-while (number != 0) {
-int digit = number % 10;
-reverse = reverse * 10 + digit;
-number = number / 10;
-}
-if (original == reverse) {
-System.out.println(original + " is a Palindrome.");
-} else {
-System.out.println(original + " is not a Palindrome.");
-}
-sc.close();
-}
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Please provide a number.");
+            return;
+        }
+
+        int number = Integer.parseInt(args[0]);
+        int original = number;
+        int reverse = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number = number / 10;
+        }
+
+        if (original == reverse) {
+            System.out.println(original + " is a Palindrome.");
+        } else {
+            System.out.println(original + " is not a Palindrome.");
+        }
+    }
 }
